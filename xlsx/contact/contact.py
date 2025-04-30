fd6a0c911c4a39ab7f706b094bb5fe5fda60bdb5
Matthew Lipper <mlipper@gmail.com>
2025-04-30 15:45:35 -0400

    Documentation and cleanup.

--- xlsx/contact/contact.py
+++ xlsx/contact/contact.py
@@ -1,26 +1,47 @@
-import logging
-import time
+"""
+Extracts contact information from Excel request forms.
 
-from collections.abc import Iterable
+Distinguishes between v1 and v2 signup forms because contact information
+exists in different cell locations:
+
+form v1:
+  B16:E20
+  Bxx - name
+  Cxx - email
+  Dxx - phone
+  Exx - role
+    
+form v2:
+  C25:F29
+  Cxx - name
+  Dxx - email
+  Exx - phone
+  Fxx - role
+"""
+import logging
+
 from dataclasses import dataclass, field
 from pathlib import Path
-from re import match
 from openpyxl.reader.excel import load_workbook
-from openpyxl.workbook import Workbook
 from openpyxl.worksheet.worksheet import Worksheet
 
 
 _log = logging.getLogger(__name__)
 
 
-APP_COLUMNS = ["C","D","E","F"]
-APP_ROWS = [25, 26, 27, 28, 29]
-FORM_CURRENT = "Geoclient API"
-FORM_LEGACY = "Geoclient API - v1"
-MISSING_EMAIL = "N/A"
+APP_COLUMNS_V1 = ["B", "C", "D", "E"]
+APP_COLUMNS_V2 = ["C", "D", "E", "F"]
+APP_ROWS_V1 = [16, 17, 18, 19, 20]
+APP_ROWS_V2 = [25, 26, 27, 28, 29]
+FORM_V1 = "Geoclient API - v1"
+FORM_V2 = "Geoclient API"
+INPUT_DIR = "files"
+OUTPUT_DIR = "out"
 ROLE_SPONSOR = "Sponsor"
 ROLE_SUPPORT = "Support"
 ROLE_TEAM_DEFAULT = "Team"
+SHEET_NAME = "Request Form"
+VERSION_CELL = "B3"
 
 
 @dataclass
@@ -38,21 +59,7 @@ class Contact:
 class Application:
     """Container for an application's contact information."""
     name: str
-    group: Contact = None
     team: list[Contact] = field(default_factory=list)
-    sponsor: Contact = None
-
-def contains_email(text):
-    pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-    if match(pattern, text):
-        return True
-    else:
-        return False
-
-def get_xlsx_paths():
-    directory = Path("./source/xlsx/")
-    xlsx_files = sorted(directory.rglob("*.xlsx"))
-    return xlsx_files
 
 def _cval(ws: Worksheet, cell: str, default_value=None):
     if ws is None:
@@ -68,7 +75,7 @@ def _cval(ws: Worksheet, cell: str, default_value=None):
 def _get_group(ws: Worksheet):
     """
     Gets the distribution list contact info.
-    Only applies to the current form.
+    Only applies to the v2 form.
 
     C23 - group email
     D23 - group name
@@ -82,7 +89,7 @@ def _get_group(ws: Worksheet):
     -------
     Contact
     """
-    contact = Contact(_cval(ws, "C23", MISSING_EMAIL), _cval(ws, "D23"), _cval(ws, "E23"), ROLE_SUPPORT)
+    contact = Contact(_cval(ws, "C23"), _cval(ws, "D23"), _cval(ws, "E23"), ROLE_SUPPORT)
     if not contact.has_info():
         return None
     return contact
@@ -90,7 +97,7 @@ def _get_group(ws: Worksheet):
 def _get_sponsor(ws: Worksheet):
     """
     Gets the project sponsor contact info.
-    Only applies to the current form.
+    Only applies to the v2 form.
 
     C18 - name
     D18 - email
@@ -105,45 +112,47 @@ def _get_sponsor(ws: Worksheet):
     -------
     Contact
     """
-    contact = Contact(_cval(ws, "D18", MISSING_EMAIL), _cval(ws, "C18"), _cval(ws, "E18"), _cval(ws, "F18", ROLE_SPONSOR))
+    contact = Contact(_cval(ws, "D18"), _cval(ws, "C18"), _cval(ws, "E18"), _cval(ws, "F18", ROLE_SPONSOR))
     if not contact.has_info():
         return None
     return contact
 
-def _get_team(ws: Worksheet):
+def _get_team(ws: Worksheet, form: str = FORM_V2):
     """
-    Gets the project application team members contact info.
-    Only applies to the current form.
-
-    C25:F29
-    Cxx - name
-    Dxx - email
-    Exx - phone
-    Fxx - role
+    For the v2 form, extracts the application team members contact info.
+    For the v1 form, extracts all contact info.
 
     Parameters
     ----------
-    ws: Worksheet
+    ws: Worksheet containing the populated form. 
+    form: Form version 1 or 2 (default).
 
     Returns
     -------
     list of Contact
     """
+    rows = APP_ROWS_V2
+    columns = APP_COLUMNS_V2
+
+    if form == FORM_V1:
+        rows = APP_ROWS_V1
+        columns = APP_COLUMNS_V1
+
     team_members = []
-    for row in APP_ROWS:
+    for row in rows:
         email = None
         name = None
         phone = None
         role = None
-        for col in APP_COLUMNS:
-            match col:
-                case "C":
+        for col in columns:
+            match columns.index(col):
+                case 0:
                     name = _cval(ws,f"{col}{row}")
-                case "D":
-                    email = _cval(ws,f"{col}{row}", MISSING_EMAIL)
-                case "E":
+                case 1:
+                    email = _cval(ws,f"{col}{row}")
+                case 2:
                     phone = _cval(ws,f"{col}{row}")
-                case "F":
+                case 3:
                     role = _cval(ws,f"{col}{row}", ROLE_TEAM_DEFAULT)
                 case _:
                     raise ValueError(f"Unexpected column {col}.")
@@ -152,38 +161,51 @@ def _get_team(ws: Worksheet):
             team_members.append(contact)
     return team_members
 
-def export(excel_file: Path):
+def slurp(excel_file: Path):
     """
-    Extracts contact information from Excel file.
-
-    Distinguishes between a "current" signup form and a "legacy" (v1) form
-    in order to extract data from the proper cell locations.
-    
-    If B3 contains "Geoclient API - v1",
-    then it is a legacy form. If B3 is "Geoclient API", then it is the
-    current form.
+    Extracts contact information from the given Excel file.
     """
     wb = load_workbook(excel_file, True, False, True)
-    ws = wb["Request Form"]
-    #print("------------------------------------------------------------------------------")
-    if ws["B3"].value == FORM_CURRENT:
+    ws = wb[SHEET_NAME]
+
+    team = None
+    if ws[VERSION_CELL].value == FORM_V2:
         sponsor = _get_sponsor(ws)
         team = _get_team(ws)
         group = _get_group(ws)
-        app = Application(excel_file.stem, group, team, sponsor)
-        print(app)
-    elif ws["B3"].value == FORM_LEGACY:
-        print(f"{excel_file.stem} - legacy form")
+        if group is not None and group.has_info():
+            team.append(group)
+        if sponsor is not None and sponsor.has_info():
+            team.append(sponsor)
+    elif ws[VERSION_CELL].value == FORM_V1:
+        team = _get_team(ws, FORM_V1)
     else:
-        print(f"{excel_file.stem} - ERROR")
+        logging.error("Unrecogized form version %s for %s.", ws[VERSION_CELL], excel_file.stem)
+        team = []
+
+    return Application(excel_file.stem, team)
+
+def _write_report(apps: list[Application]):
+    for app in apps:
+        logging.info("%s:", app.name)
+        for contact in app.team:
+            logging.info("  %s, %s, %s, %s", contact.email, contact.name, contact.phone, contact.role)
 
 def main():
-    logging.basicConfig(level=logging.WARNING)
+    logging.basicConfig(level=logging.INFO)
 
-    xlsx_paths = get_xlsx_paths()
-    for excel_file in xlsx_paths:
-        export(excel_file)
+    cwd = Path(__file__).parent
+    input_dir = cwd / INPUT_DIR
+    xlsx_files = sorted(input_dir.rglob("*.xlsx"))
+
+    apps = []
+    for excel_file in xlsx_files:
+        app = slurp(excel_file)
+        apps.append(app)
+    _write_report(apps)
 
 
 if __name__ == "__main__":
+    print("Hello")
     main()
+    print("Goodbye")
